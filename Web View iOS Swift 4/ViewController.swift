b3c769284b9d2702da17c4262a8c3a77827440cd
Kostia Kovba <konstantinkovba@gmail.com>
2018-01-28 19:49:56 +0200

    Initial Commit

---
+++ Web View iOS Swift 4/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Web View iOS Swift 4
+//
+//  Created by Kostia Kovba on 28.01.18.
+//  Copyright © 2018 Kostia Kovba. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
